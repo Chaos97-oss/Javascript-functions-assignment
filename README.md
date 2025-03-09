e09162697d6142975682a25f9d14b541fd5973c7
Paul <udegbuepaul43@gmail.com>
2025-03-09 15:50:55 +0100

    unleash

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # javascript-functions-assignment
+## Assignment Overview
+
+This assignment consists of multiple JavaScript functions designed to perform various operations, such as determining if a number is even, reversing a string, and categorizing a student's score. The goal is to demonstrate fundamental JavaScript programming skills.
+## Instructions
+1.  Clone the repository: `git clone [https://github.com/Chaos97-oss/javascript-functions-assignment.git]`
+
+2.  Open the `index.html` file in your browser.
+3.  View the results in the console or on the webpage.
+## Functions
+
+### calculateArea(length, width)
+
+Calculates the area of a rectangle.
+
+**Example:**
+
+```javascript
+calculateArea(5, 10); // Returns 50
+
+
+reverseString(str)
+Reverses a string.
+JavaScript
+reverseString("hello"); // Returns "olleh
+
+
+
--- index.js
+++ index.js
@@ -62,5 +62,47 @@ let result = laugh.repeat(3);
 console.log(result);
 
 
+//Calculate Area Function
+function calculateArea (length, width) {
+  return length * width;
+}
+console.log(calculateArea(3.5, 2));
+
+
+// String Reverser
+function reverseString(str) {
+  return str.split("").reverse().join("")
+}
+console.log(reverseString("hello"));
+
+
+//Even number checker
+function isEven(number) {
+  return number % 2 === 0; 
+} 
+console.log(isEven(4));
+console.log(isEven(7));
+
+
+//Score Categorizer
+function scoreCategory(score) {
+  if (score >= 90) {
+    return "Excellent";
+  } else if (score >= 80) {
+    return "Very good";
+  } else if (score >= 70){
+    return "Pass";
+  } else {
+    return "fail";
+  } 
+}
+console.log(scoreCategory(99));
+console.log(scoreCategory(87));
+console.log(scoreCategory(75));
+console.log(scoreCategory(60));
+
+
+
+
 
 
